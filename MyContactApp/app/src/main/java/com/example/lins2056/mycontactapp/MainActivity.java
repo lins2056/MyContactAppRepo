b2608838bd184560facb15a4e24448308841a347
lins2056 <LinS2056@sduhsd.lan>
2017-05-15 09:15:22 -0700

    Added addData method to add the inputted name to database

--- MyContactApp/app/src/main/java/com/example/lins2056/mycontactapp/MainActivity.java
+++ MyContactApp/app/src/main/java/com/example/lins2056/mycontactapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.lins2056.mycontactapp;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -12,7 +14,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper myDb;
-    EditText editname;
+    EditText editName;
     Button btnAddData;
 
 
@@ -22,5 +24,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_name);
+    }
+
+    public void addData(View v) {
+        boolean isInserted = myDb.insertData(editName.getText().toString());
+        if(isInserted == true){
+            Log.d("MyContact", "Data insertion successful");
+            //create toast message to user indicating data inserted correctly
+
+        }
+        else{
+            Log.d("MyContact", "Data insertion unsuccessful");
+            //create toast message to user indicating data inserted incorrectly
+
+        }
+
     }
 }
